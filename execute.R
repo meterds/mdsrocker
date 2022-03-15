fcb0afafb5fc4c7cd97e2f955d0964225db8b1cf
Christoph Stepper <christoph.stepper@metergroup.com>
2022-03-15 08:19:12 +0100

    more tests :)

--- execute.R
+++ execute.R
@@ -6,9 +6,9 @@ purrr::pwalk(mdsrocker::mdsrocker_installation, mdsrocker::create_shellscript)
 
 # ## dockerfiles ----
 # purrr::pwalk(mdsrocker::mdsrocker_dockerfiles, mdsrocker::create_dockerfile)
-
+#
 # ## github actions ----
 # mdsrocker::create_action_workflow(
-#   account = desc::desc_get_field("Config/Dockerhub/Account"),
-#   images = mdsrocker::mdsrocker_dockerfiles$image
+#  account = desc::desc_get_field("Config/Dockerhub/Account"),
+#  images = mdsrocker::mdsrocker_dockerfiles$image
 # )
