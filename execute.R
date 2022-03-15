0b3a922b67a9d4dc494eb535541f16bbf62bb68e
Christoph Stepper <christoph.stepper@metergroup.com>
2022-03-15 09:38:59 +0100

    redo all execute.R

--- execute.R
+++ execute.R
@@ -1,16 +1,14 @@
 # ## ensure that latest version of package data is considered ----
 # devtools::load_all(".")
 
-remotes::package_deps("rcmdcheck")
+## shell scripts ----
+purrr::pwalk(mdsrocker::mdsrocker_installation, mdsrocker::create_shellscript)
 
-# ## shell scripts ----
-# purrr::pwalk(mdsrocker::mdsrocker_installation, mdsrocker::create_shellscript)
-#
-# ## dockerfiles ----
-# purrr::pwalk(mdsrocker::mdsrocker_dockerfiles, mdsrocker::create_dockerfile)
-#
-# ## github actions ----
-# mdsrocker::create_action_workflow(
-#  account = desc::desc_get_field("Config/Dockerhub/Account"),
-#  images = mdsrocker::mdsrocker_dockerfiles$image
-# )
+## dockerfiles ----
+purrr::pwalk(mdsrocker::mdsrocker_dockerfiles, mdsrocker::create_dockerfile)
+
+## github actions ----
+mdsrocker::create_action_workflow(
+ account = desc::desc_get_field("Config/Dockerhub/Account"),
+ images = mdsrocker::mdsrocker_dockerfiles$image
+)
