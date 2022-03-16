22abfb3256961f2d335826b303ca3661cfeecc6f
Christoph Stepper <christoph.stepper@metergroup.com>
2022-03-16 13:04:23 +0100

    execute.R: restructure

--- execute.R
+++ execute.R
@@ -1,19 +1,27 @@
+# read relevant information from DESCRIPTION ----
+account = desc::desc_get_field("Config/Dockerhub/Account")
+rversions = desc::desc_get_list("Config/Docker/Rversions")
+
+# generate scripts ----
+
 ## shell scripts ----
-purrr::pwalk(mdsrocker::mdsrocker_installation, mdsrocker::create_shellscript)
+purrr::pwalk(
+  mdsrocker::mdsrocker_installation,
+  mdsrocker::create_shellscript
+)
 
 ## dockerfiles ----
-rversions = desc::desc_get_list("Config/Docker/Rversions")
 for (rver in rversions) {
   purrr::pwalk(
     mdsrocker::mdsrocker_dockerfiles,
     mdsrocker::create_dockerfile,
-    tag = rver)
+    tag = rver
+  )
 }
 
-## github actions ----
-rversions = desc::desc_get_list("Config/Docker/Rversions")
+# github actions workflow ----
 mdsrocker::create_action_workflow_publish_docker_images(
- account = desc::desc_get_field("Config/Dockerhub/Account"),
- images = mdsrocker::mdsrocker_dockerfiles$image,
- rversions = rversions
+  account = account,
+  images = mdsrocker::mdsrocker_dockerfiles$image,
+  rversions = rversions
 )
